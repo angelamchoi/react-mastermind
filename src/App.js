0219a05ee7a3ce51cd8915295dc7176dd0f19dc0
Angela Choi <angelamchoi@gmail.com>
2021-07-10 17:26:27 -0400

    added components folders

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
+import GameBoard from './components/GameBoard/GameBoard'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">React Mastermind</header>
+        <GameBoard />
       </div>
     );
   }
 }
 
 export default App;
+
+//--------NOTES-------//
+// Key Class
+//-- must have a render method
+//-- needs a function 
+//-- uses class extends
+//-- uses this keyword (this.props.earthlings)
+//-- NO COMMAS 
